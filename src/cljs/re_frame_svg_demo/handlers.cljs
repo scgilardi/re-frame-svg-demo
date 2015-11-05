9a393702a30da59db5581e2a2ba4f20091e5129e
Stephen C. Gilardi <scgilardi@gmail.com>
2015-11-05 09:36:26 -0500

    simplify mouse handler

--- src/cljs/re_frame_svg_demo/handlers.cljs
+++ src/cljs/re_frame_svg_demo/handlers.cljs
@@ -14,23 +14,22 @@
 
 (f/register-handler
  :mouse-event
- (fn [{:keys [next-id drag] :as db} [_ event]]
-   (let [{:keys [type pos buttons modifiers]} event
-         button-state (if drag :was-down :was-up)]
+ (fn [{drag :drag :as db} [_ {:keys [type pos] :as event}]]
+   (let [button-state (if drag :was-down :was-up)]
      (case [type button-state]
        [:mousedown :was-up]
        (assoc db :drag {:start pos :pos pos})
        [:mousemove :was-down]
-       (assoc db :drag (assoc drag :pos pos))
+       (assoc-in db [:drag :pos] pos)
        [:mouseup :was-down]
        (do
-         (f/dispatch [:cleanup-drag])
+         (f/dispatch [:finish-drag])
          (f/dispatch [:create-item (assoc drag :pos pos)])
          db)
        db))))
 
 (f/register-handler
- :cleanup-drag
+ :finish-drag
  (fn [db [_]]
    (dissoc db :drag)))
 
